64907c1236cac1778f7f10f563091d60cb1f3563
InSeongSo <goflvhxj2547@gmail.com>
2022-01-23 12:23:32 +0900

    ✒️Feat: 메뉴 수정, 품절/입고, 삭제 기능 추가

--- packages/inseong-so/src/components/menuPage/MenuPageContainer.ts
+++ packages/inseong-so/src/components/menuPage/MenuPageContainer.ts
@@ -1,7 +1,8 @@
+import { TMenuProps, TMenuEventHandler } from 'App';
 import { TMouseEvent } from 'DOMEvent';
 import { IStore } from 'Store';
 import { ERROR_MESSAGE } from '~/src/constants';
-import { createUUID } from '~/src/helpers/common';
+import { $closets, createUUID } from '~/src/helpers/common';
 import MenuPage from './MenuPage';
 
 // let lastCallback: any;
@@ -28,30 +29,68 @@ const MenuPageContainer = (store: IStore) => {
   const { id, text } = store.read('current');
   const menuListId = `${id}-menuList`;
 
-  const addMenu = () => {
-    const { id: currentId } = store.read('current');
-    const currentMenuListId = `${currentId}-menuList`;
-
+  const addMenu = (menuList: TMenuProps[], categoryId: string) => {
     const $input = document.querySelector('.input-field') as HTMLInputElement;
     if ($input.value.trim() === '') return alert(ERROR_MESSAGE.INVALIID_INPUT);
-
-    const menuList = store.read(currentMenuListId);
     menuList.push({ menuId: createUUID(), name: $input.value, isSoldOut: false });
-    store.create(currentMenuListId, menuList);
+    store.create(categoryId, menuList);
 
     $input.value = '';
   };
 
-  const soldOutMenu = () => {};
-  const editMenu = () => {};
-  const removeMenu = () => {};
+  const soldOutMenu: TMenuEventHandler = ($element, menuList, categoryId) => {
+    const isSoldOut = $element.classList.contains('sold-out');
+    const key = $element.getAttribute('key');
+    if (!confirm(`${isSoldOut ? '입고' : '품절'} 처리를 하시겠어요?`)) return;
+    const newMenuList = menuList.map(menu => {
+      if (menu.menuId === key) return { ...menu, isSoldOut: !isSoldOut };
+      return menu;
+    });
+    store.create(categoryId, newMenuList);
+  };
+  const editMenu: TMenuEventHandler = ($element, menuList, categoryId) => {
+    const newMenuName = prompt('메뉴를 수정하시겠어요?') as string;
+    if (newMenuName.length < 1) return alert(ERROR_MESSAGE.INVALIID_INPUT);
+    const key = $element.getAttribute('key');
+    const newMenuList = menuList.map(menu => {
+      if (menu.menuId === key) return { ...menu, name: newMenuName };
+      return menu;
+    });
+    store.create(categoryId, newMenuList);
+  };
+  const removeMenu: TMenuEventHandler = ($element, menuList, categoryId) => {
+    if (!confirm('메뉴를 삭제하시겠어요?')) return;
+    const key = $element.getAttribute('key');
+    const newMenuList = menuList.filter(menu => menu.menuId !== key);
+    store.create(categoryId, newMenuList);
+  };
+
+  const SELECTOR = {
+    ADD: '.input-submit',
+    SOLDOUT: '.menu-sold-out-button',
+    EDIT: '.menu-edit-button',
+    REMOVE: '.menu-remove-button',
+  };
 
   // TODO: 이벤트 리스너를 어떻게 효율적으로 처리할 것인가
   const handleClick = ({ target }: TMouseEvent) => {
-    if (target.matches('.input-submit')) return addMenu();
-    if (target.matches('.menu-sold-out-button')) return soldOutMenu();
-    if (target.matches('.menu-edit-button')) return editMenu();
-    if (target.matches('.menu-remove-button')) return removeMenu();
+    const $item = $closets(target, 'li', 'span');
+    if ($item === undefined) return;
+    const { id: currentId } = store.read('current');
+    const currentMenuListId = `${currentId}-menuList`;
+    const menuList = store.read(currentMenuListId) as TMenuProps[];
+    if (target.matches(SELECTOR.ADD)) {
+      return addMenu(menuList, currentMenuListId);
+    }
+    if (target.matches(SELECTOR.SOLDOUT)) {
+      return soldOutMenu($item, menuList, currentMenuListId);
+    }
+    if (target.matches(SELECTOR.EDIT)) {
+      return editMenu($item, menuList, currentMenuListId);
+    }
+    if (target.matches(SELECTOR.REMOVE)) {
+      return removeMenu($item, menuList, currentMenuListId);
+    }
   };
 
   return {
