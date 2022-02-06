e4d263def93a6ec16d51f9d04802ae8573abc42c
Mihyun <xianemml@gmail.com>
2022-02-06 21:54:31 +0900

    fix: 메뉴 카운터 수정, 불필요한 helper 함수 삭제

--- packages/xianeml/src/components/MenuCount.ts
+++ packages/xianeml/src/components/MenuCount.ts
@@ -1,14 +1,10 @@
 import { Tstate } from '../types/store.js';
-import { getCategoryMenus } from '../utils/helper.js';
 
-const MenuCount = (state: Tstate) => {
-  const { menus, currentTab } = state;
-  const categoryMenus = getCategoryMenus(menus, currentTab);
-
+const MenuCount = ({ menus, currentTab }: Tstate) => {
   return `
     <h2 class="mt-1">${currentTab.name} 메뉴 관리</h2>
     <span id="menu-count" class="mr-2 mt-4 menu-count">
-      총 ${categoryMenus.length}개
+      총 ${menus.length}개
     </span>`;
 };
 
--- packages/xianeml/src/utils/helper.ts
+++
@@ -1,5 +0,0 @@
-import { Tmenu, Tcategory } from '../types/store.js';
-
-export const getCategoryMenus = (menus: Tmenu[], currentTab: Tcategory) => {
-  return menus.filter(menu => menu.categoryId === currentTab.id);
-};
