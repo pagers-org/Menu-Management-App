af9e1a3caf9a18d17533f131c5963edfd3caa24d
InSeongSo <goflvhxj2547@gmail.com>
2022-02-09 23:43:45 +0900

    🧺Remove: 다음 프로젝트 진행을 위한 코드 제거

--- packages/inseong-so/cypress/plugins/index.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on, config) => {
-  //
-};
--- packages/inseong-so/src/redux/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from '../common';
-import menus from './menus';
-
-const rootReducer = combineReducers({ menus });
-
-export default rootReducer;
