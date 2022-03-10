0493220b870cd3ea3244b9c16952751962496786
Mihyun <xianemml@gmail.com>
2022-03-10 22:53:02 +0900

    chore: vue-vite 프로젝트 세팅, 실행 스크립트 추가 #40

---
+++ packages/xianeml-vue-vite/src/constants/index.ts
@@ -0,0 +1,7 @@
+export const INITIAL_CATEGORIES = [
+  { id: 'espresso', text: '☕ 에스프레소', selected: true, menus: [] },
+  { id: 'frappuccino', text: '🥤 프라푸치노', selected: false, menus: [] },
+  { id: 'blended', text: '🍹 블렌디드', selected: false, menus: [] },
+  { id: 'teavana', text: '🍸 티바나', selected: false, menus: [] },
+  { id: 'desert', text: '🍰 디저트', selected: false, menus: [] },
+];
